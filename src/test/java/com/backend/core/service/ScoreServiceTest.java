39ca2a71fa342464539f35e69c44fe2214bcc0cd
Carlos Javier Mena <mena@strands.com>
2018-04-25 15:45:33 +0200

    tests

--- src/test/java/com/backend/core/service/ScoreServiceTest.java
+++ src/test/java/com/backend/core/service/ScoreServiceTest.java
@@ -1,5 +1,6 @@
 package com.backend.core.service;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,10 +18,22 @@ public class ScoreServiceTest {
 
 
     @Test
-    public void testSaveScoresByOneLevel() throws Exception {
-        for(int i=1;i<15;i++){
-            scoreService.saveScore(1,i, ThreadLocalRandom.current().nextInt(1, 1000 + 1));
+    public void testSaveScore() throws Exception {
+        int levelId = 1;
+        int userId = 78;
+        int score = 12;
+        scoreService.saveScore(levelId,userId,score);
+        String userScore = scoreService.getUserScoresByLevel(levelId).toString();
+        Assert.assertEquals("User Score", "{78=12}", userScore);
+    }
+    @Test
+    public void testSaveUserScoresInTheSameLevel() throws Exception {
+        int levelId = 1;
+        for(int i=1;i<3;i++){
+                scoreService.saveScore(levelId,i, i);
         }
+        String userScore = scoreService.getUserScoresByLevel(levelId).toString();
+        Assert.assertEquals("User Scores", "{1=1, 2=2}", userScore);
     }
     @Test
     public void testSaveScoresBySeveralLevelsAndCheckMaxSize() throws Exception {
